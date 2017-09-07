5d3024dae4cb692c693c7c7fca127787afbfeab4
Erick Lui <erickjin.lui@gmail.com>
2017-09-07 11:14:00 -0700

    fixed error when finding players that never competed

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -8,41 +8,43 @@ import { HttpService } from './http.service';
 })
 export class AppComponent {
   platform: [boolean] = [true, false, false]
-  user_id: string = '';
-  platform_id: string = '1';
+  user_id: string = ''; // user_id is input from search to be passed to server
+  platform_id: string = '1';  // defaults to search steam platform
+  found: boolean;
+  unranked: string = '/assets/images/ranks/unranked.png';
+
   stats = {
     'name': '',
     'avatar': '',
     'solo': {
-      'tier': '',
-      'division': '',
-      'matches': '',
+      'tier': 'Unranked',
+      'icon': this.unranked,
+      'division': 'Division I',
+      'matches': 0,
       'rating': 0,
     },
     'doubles': {
-      'tier': '',
-      'division': '',
-      'matches': '',
+      'tier': 'Unranked',
+      'icon': this.unranked,
+      'division': 'Division I',
+      'matches': 0,
       'rating': 0,
     },
     'solo_standard': {
-      'tier': '',
-      'division': '',
-      'matches': '',
+      'tier': 'Unranked',
+      'icon': this.unranked,
+      'division': 'Division I',
+      'matches': 0,
       'rating': 0,
     },
     'standard': {
-      'tier': '',
-      'division': '',
-      'matches': '',
+      'tier': 'Unranked',
+      'icon': this.unranked,
+      'division': 'Division I',
+      'matches': 0,
       'rating': 0,
     },
   }
-
-  found_data = {
-    'signatureUrl': 0,
-    'code': 0
-  };
 
   constructor(
     private _http: HttpService,
@@ -51,11 +53,12 @@ export class AppComponent {
   onSubmit(form) {
     this._http.passID({'uid': this.user_id, 'pid': this.platform_id})
     .then(obj => {
-      console.log(obj);
-      this.found_data = obj;
+      console.log('API DATA', obj);
+      this.found = true;
       this.setUserStats(obj);
     })
-    .catch(err => { this.found_data.code = 1; })
+    .catch(err => { console.log('ERROR -->', err);
+    ; })
   }
 
   setPlatform(num) {
@@ -76,52 +79,68 @@ export class AppComponent {
 
     this.stats.name = obj.displayName;
     this.stats.avatar = obj.avatar;
+
+    if (season5['10']){
+      var tier_data = this.getTier(season5['10']['tier']);
+      this.stats.solo.tier = tier_data.title;
+      this.stats.solo.icon = tier_data.icon;
+      this.stats.solo.division = this.getDivision(season5['10']['division']);
+      this.stats.solo.matches = season5['10']['matchesPlayed'];
+      this.stats.solo.rating = season5['10']['rankPoints'];
+    }
     
-    this.stats.solo.tier = this.getTier(season5['10']['tier']);
-    this.stats.solo.division = this.getDivision(season5['10']['division']);
-    this.stats.solo.matches = season5['10']['matchesPlayed'];
-    this.stats.solo.rating = season5['10']['rankPoints'];
+    if (season5['11']) {
+      var tier_data = this.getTier(season5['11']['tier']);
+      this.stats.doubles.tier = tier_data.title;
+      this.stats.doubles.icon = tier_data.icon;
+      this.stats.doubles.division = this.getDivision(season5['11']['division']);
+      this.stats.doubles.matches = season5['11']['matchesPlayed'];
+      this.stats.doubles.rating = season5['11']['rankPoints'];
+    }
 
-    this.stats.doubles.tier = this.getTier(season5['11']['tier']);
-    this.stats.doubles.division = this.getDivision(season5['11']['division']);
-    this.stats.doubles.matches = season5['11']['matchesPlayed'];
-    this.stats.doubles.rating = season5['11']['rankPoints'];
+    if (season5['12']) {
+      var tier_data = this.getTier(season5['12']['tier']);
+      this.stats.solo_standard.tier = tier_data.title;
+      this.stats.solo_standard.icon = tier_data.icon;
+      this.stats.solo_standard.division = this.getDivision(season5['12']['division']);
+      this.stats.solo_standard.matches = season5['12']['matchesPlayed'];
+      this.stats.solo_standard.rating = season5['12']['rankPoints'];
+    }
 
-    this.stats.solo_standard.tier = this.getTier(season5['12']['tier']);
-    this.stats.solo_standard.division = this.getDivision(season5['12']['division']);
-    this.stats.solo_standard.matches = season5['12']['matchesPlayed'];
-    this.stats.solo_standard.rating = season5['12']['rankPoints'];
-
-    this.stats.standard.tier = this.getTier(season5['13']['tier']);
-    this.stats.standard.division = this.getDivision(season5['13']['division']);
-    this.stats.standard.matches = season5['13']['matchesPlayed'];
-    this.stats.standard.rating = season5['13']['rankPoints'];
-
-    console.log(this.stats);
+    if (season5['13']) {
+      tier_data = this.getTier(season5['13']['tier']);
+      this.stats.standard.tier = tier_data.title;
+      this.stats.standard.icon = tier_data.icon;
+      this.stats.standard.division = this.getDivision(season5['13']['division']);
+      this.stats.standard.matches = season5['13']['matchesPlayed'];
+      this.stats.standard.rating = season5['13']['rankPoints'];
+    }
+    console.log('FOUND STATS', this.stats)
   }
 
   getTier(key) {
+    let assets = '/assets/images/ranks/';
     let dict = {
-      '0': 'Unranked',
-      '1': 'Bronze I',
-      '2': 'Bronze II',
-      '3': 'Bronze III',
-      '4': 'Silver I',
-      '5': 'Silver II',
-      '6': 'Silver III',
-      '7': 'Gold I',
-      '8': 'Gold II',
-      '9': 'Gold III',
-      '10': 'Platinum I',
-      '11': 'Platinum II',
-      '12': 'Platinum III',
-      '13': 'Diamond I',
-      '14': 'Diamond II',
-      '15': 'Diamond III',
-      '16': 'Champion I',
-      '17': 'Champion II',
-      '18': 'Champion III',
-      '19': 'Grand Champion'
+      '0': { 'title': 'Unranked', 'icon': assets + 'unranked.png'},
+      '1': { 'title': 'Bronze I', 'icon': assets + 'b1.png' },
+      '2': { 'title': 'Bronze II', 'icon': assets + 'b2.png' },
+      '3': { 'title': 'Bronze III', 'icon': assets + 'b3.png' },
+      '4': { 'title': 'Silver I', 'icon': assets + 's1.png' },
+      '5': { 'title': 'Silver II', 'icon': assets + 's2.png' },
+      '6': { 'title': 'Silver III', 'icon': assets + 's3.png' },
+      '7': { 'title': 'Gold I', 'icon': assets + 'g1.png' },
+      '8': { 'title': 'Gold II', 'icon': assets + 'g2.png' },
+      '9': { 'title': 'Gold III', 'icon': assets + 'g3.png' },
+      '10': { 'title': 'Platinum I', 'icon': assets + 'p1.png' },
+      '11': { 'title': 'Platinum II', 'icon': assets + 'p2.png' },
+      '12': { 'title': 'Platinum III', 'icon': assets + 'p3.png' },
+      '13': { 'title': 'Diamond I', 'icon': assets + 'd1.png' },
+      '14': { 'title': 'Diamond II', 'icon': assets + 'd2.png' },
+      '15': { 'title': 'Diamond III', 'icon': assets + 'd3.png' },
+      '16': { 'title': 'Champion I', 'icon': assets + 'c1.png' },
+      '17': { 'title': 'Champion II', 'icon': assets + 'c2.png' },
+      '18': { 'title': 'Champion III', 'icon': assets + 'c3.png' },
+      '19': { 'title': 'Grand Chamption', 'icon': assets + 'gc.png' },
     }
     return dict[key];
   }
